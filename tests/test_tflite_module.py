e5e85344b52348229389ca5a8f8dc5c30762b500
王振华 (Zhenhua WANG) <i@jackwish.net>
2019-09-20 15:28:41 +0800

    init test and source

--- tests/test_tflite_module.py
+++ tests/test_tflite_module.py
@@ -1,1 +1,15 @@
 import tflite_parser.tflite as tflite
+import tflite_parser
+print(dir(tflite_parser))
+import tflite_parser.tflite as tflite
+print(dir(tflite_parser.tflite))
+import tflite_parser.tflite.Model
+print(dir(tflite_parser.tflite.Model))
+
+# from tflite_parser.tflite.Model import Model
+
+
+# from tflite.BuiltinOperator import BuiltinOperator
+# tflite = tflite_parser.tflite
+from tflite_parser.tflite.Model import Model
+print(dir(Model))
