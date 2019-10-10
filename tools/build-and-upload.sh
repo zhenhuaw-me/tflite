16022ab8d7c26d711c936c5594b171cc6b29d13c
王振华 (Zhenhua WANG) <i@jackwish.net>
2019-10-10 16:43:16 -0600

    tools: update update-schema.sh

--- tools/build-and-upload.sh
+++ tools/build-and-upload.sh
@@ -7,20 +7,20 @@ python3 ${root_dir}/setup.py bdist_wheel \
   --dist-dir ${root_dir}/assets/dist
 rm -rf tflite.egg-info
 
-read -p "Would you like to upload the package? [Y|N] " input_str
+read -p "Would you like to upload the package [Y|N] ? " input_str
 if [ -z "${input_str}" -o "${input_str}" != "Y" ]; then
   exit 0
 fi
 
-read -p "Will upload to test.pypi.org, type \"Release\" for real publishment " input_str
+read -p "Will upload to test.pypi.org, for real publishment type \"Release\": " input_str
 if [ -z "${input_str}" -o ${input_str} != "Release" ]; then
   python3 -m twine upload \
     --repository-url https://test.pypi.org/legacy/ \
     ${root_dir}/assets/dist/tflite-*
 else
-  read -p "Will publish the package, are you sure to continue? [Y|N] " input_str
-  if [ -z "${input_str}" -o ${input_str} = "Y" ]; then
+  read -p "Will publish the package, are you sure to continue [Y|N] ? " input_str
+  if [ -n "${input_str}" -a ${input_str} = "Y" ]; then
     echo "Uploading..."
-    # python3 -m twine upload ${root_dir}/assets/dist/tflite-*
+    python3 -m twine upload ${root_dir}/assets/dist/tflite-*
   fi
 fi
--- tools/update-schema.sh
+++ tools/update-schema.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]; then
-  echo "Usage: update-schema.sh [TensorFlow Version]"
-  echo "    Download schema and generate python module"
-  echo "Note: you may need to change the flatbuffers complier 'FBSC'"
+# which version?
+read -p "Which version of TensorFlow would you like to obtain the schema (\"v1.14.0\" for example): " version
+if [ -z ${version} ]; then
   exit 1
 fi
+read -p "Going to obtain schema from version \"${version}\", continue? [Y|N] " input_str
+if [ -z ${input_str} -o "${input_str}" != "Y"  ]; then
+  exit 0
+fi
 
-version=${1}
 
 # If you don't have a flatbuffers complier, you may build one with guide:
 # https://google.github.io/flatbuffers/flatbuffers_guide_building.html
@@ -15,13 +17,14 @@ FBSC=~/toolchain/flatbuffers/flatbuffers/build/flatc
 
 root_dir=$(dirname $(dirname $(readlink -f $0})))
 
-schema_path="${root_dir}/3rdparty/tflite_schema/schema.fbs"
+schema_path="${root_dir}/3rdparty/schema.fbs"
 output_path="${root_dir}/tflite"
 
 # download schema.fbs
 repo_tree='https://raw.githubusercontent.com/tensorflow/tensorflow'
 file_path='tensorflow/lite/schema/schema.fbs'
 url="${repo_tree}/${version}/${file_path}"
+echo "Now, downloading from ${url}"
 curl ${url} -o "${schema_path}"
 
 # exit?
@@ -38,6 +41,12 @@ fi
 echo "Building flatbuffers python module in ${output_path}"
 ${FBSC} --python -o ${output_path} ${schema_path}
 
-# commit the schema change
+# commit the schema change?
+echo "The diff of built out python module"
+echo $(git diff)
+read -p "Going to create a commit for these changes, continue? [Y|N] " input_str
+if [ -z ${input_str} -o "${input_str}" != "Y"  ]; then
+  exit 0
+fi
 git add ${schema_path} ${output_path}/tflite
 git commit -m "update schema.fbs and tflite module to ${version}"
