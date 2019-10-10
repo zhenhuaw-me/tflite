18fa02d89bda66bfd9f292ef543597889691a7e0
王振华 (Zhenhua WANG) <i@jackwish.net>
2019-10-10 16:04:40 +0800

    tools: improve build-and-upload.sh

--- tools/build-and-upload.sh
+++ tools/build-and-upload.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-testRelease=1
-if [ $# -ne 0 ]; then
-  if [ ${1} = "Release" ]; then
-    testRelease=0
-  fi
-fi
-
 root_dir=$(dirname $(dirname $(readlink -f $0})))
 
 python3 ${root_dir}/python/setup.py bdist_wheel \
@@ -14,11 +7,20 @@ python3 ${root_dir}/python/setup.py bdist_wheel \
   --dist-dir ${root_dir}/assets/dist
 rm -rf tflite.egg-info
 
-if [ ${testRelease} -eq 1 ]; then
+read -p "Would you like to upload the package? [Y|N] " input_str
+if [ -z "${input_str}" -o "${input_str}" != "Y" ]; then
+  exit 0
+fi
+
+read -p "Will upload to test.pypi.org, type \"Release\" for real publishment " input_str
+if [ -z "${input_str}" -o ${input_str} != "Release" ]; then
   python3 -m twine upload \
     --repository-url https://test.pypi.org/legacy/ \
     ${root_dir}/assets/dist/tflite-*
 else
-  echo "will upload"
-  python3 -m twine upload ${root_dir}/assets/dist/tflite-*
+  read -p "Will publish the package, are you sure to continue? [Y|N] " input_str
+  if [ -z "${input_str}" -o ${input_str} = "Y" ]; then
+    echo "Uploading..."
+    # python3 -m twine upload ${root_dir}/assets/dist/tflite-*
+  fi
 fi
