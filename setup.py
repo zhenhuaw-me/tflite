6ff6228c6877764e2e484632257743bf62fb595f
王振华 (Zhenhua WANG) <i@jackwish.net>
2020-02-11 03:35:16 -0800

    version 2.0.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(here, 'README.md'), 'r') as fh:
 
 setuptools.setup(
     name='tflite',
-    version='2.1.0',
+    version='2.0.1',
     description="A package to parse TFLite models (*.tflite)",
     packages=setuptools.find_packages(),
     classifiers=[
