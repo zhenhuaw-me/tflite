0d1e80fee889b7c1c071349862d5ccc66eb3abbd
王振华 (Zhenhua WANG) <i@jackwish.net>
2021-01-25 20:44:34 +0800

    gen doc

--- scripts/gen-doc.sh
+++ scripts/gen-doc.sh
@@ -9,7 +9,7 @@ doc_dir=${root_dir}/docs
 proj_dir=${root_dir}/tflite
 
 rm -rf ${doc_dir}
-pdoc --overwrite --html --html-dir ${doc_dir} ${proj_dir} tflite
+pdoc -f --html -o ${doc_dir} ${proj_dir} tflite
 mv ${doc_dir}/tflite/* ${doc_dir}
 rmdir ${doc_dir}/tflite
 
